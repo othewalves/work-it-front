c326a55c2d2a72a134d088999d92e77c3dac0aef
Matheus Alves <matheusfrancisco0330@gmail.com>
2025-09-01 22:29:29 -0300

    fix: alter url of api

--- src/app/api/axios.ts
+++ src/app/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://work-it-api-production.up.railway.app',
     // headers: {
     withCredentials: true,
     // "Content-Type": "application/json",
